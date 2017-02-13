2586abaa9a1879315f37d4fd232bea5640238db4
Shweta Lanetteam <lanetteam.shweta@gmail.com>
2017-02-13 17:41:28 +0530

    Initial commit

---
+++ app/src/main/java/com/firebase/ads/fragmentdemo/FragmentDemo.java
@@ -0,0 +1,22 @@
+package com.firebase.ads.fragmentdemo;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by lcom28 on 13/2/17.
+ */
+
+public class FragmentDemo extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+
+
+    }
+}
---
+++ app/src/main/java/com/firebase/ads/fragmentdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.firebase.ads.fragmentdemo;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_fragment);
+
+    }
+}
